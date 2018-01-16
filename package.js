4bd1b1b8374a5722991974b5764a2dcad8a7e0ae
Mitar <mitar.git@tnode.com>
2018-01-15 23:09:30 -0800

    Package updated to CoffeeScript 2.

--- package.js
+++ package.js
@@ -1,30 +1,31 @@
 Package.describe({
   summary: "Reactive server-side MongoDB queries",
-  version: '0.1.1',
+  version: '0.2.0',
   name: 'peerlibrary:reactive-mongo',
   git: 'https://github.com/peerlibrary/meteor-reactive-mongo.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.0.3.1');
+  api.versionsFrom('METEOR@1.4.4.5');
 
   // Core dependencies.
   api.use([
-    'coffeescript',
+    'coffeescript@2.0.3_3',
+    'ecmascript',
     'underscore',
     'mongo'
   ], 'server');
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:server-autorun@0.5.1'
+    'peerlibrary:server-autorun@0.7.0'
   ], 'server');
 
   // Package can be used without PeerDB. But if PeerDB is available, make
   // sure it is loaded before this package so that PeerDB adds "exists"
   // to the cursor before we make it reactive.
   api.use([
-    'peerlibrary:peerdb@0.19.1'
+    'peerlibrary:peerdb@0.25.0'
   ], 'server', {'weak': true});
 
   api.addFiles([
@@ -33,8 +34,11 @@ Package.onUse(function (api) {
 });
 
 Package.onTest(function (api) {
+  api.versionsFrom('METEOR@1.4.4.5');
+
   // Core dependencies.
   api.use([
+    'ecmascript',
     'tinytest',
     'test-helpers',
     'mongo',
@@ -43,12 +47,13 @@ Package.onTest(function (api) {
     'random',
     'minimongo',
     'ejson',
-    'mongo-id'
+    'mongo-id',
+    'id-map'
   ], 'server');
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:server-autorun@0.5.1'
+    'peerlibrary:server-autorun@0.7.0'
   ], 'server');
 
   // Internal dependencies.
@@ -56,10 +61,12 @@ Package.onTest(function (api) {
     'peerlibrary:reactive-mongo'
   ]);
 
-  api.add_files([
+  api.addFiles([
     // We modify environment to make Minimongo tests in fact use MongoDB database.
     'tests_prepare.js',
     'meteor/packages/minimongo/minimongo_tests.js',
+    'meteor/packages/minimongo/minimongo_tests_client.js',
+    'meteor/packages/minimongo/minimongo_tests_server.js',
     'tests.js'
   ], 'server');
 });
--- tests_prepare.js
+++ tests_prepare.js
@@ -1,6 +1,7 @@
 // We are repurposing Minimongo tests to test reactive server-side MongoDB
 // collections. So we have to do some preparations here.
 
+originalLocalCollection = LocalCollection;
 LocalCollection = function (name, options) {
   // We want always to use MongoDB, not local collections.
   name = name || Random.id();
@@ -9,6 +10,13 @@ LocalCollection = function (name, options) {
   options.idGeneration = 'MONGO';
   return new Mongo.Collection(name, options);
 };
+
+LocalCollection.wrapTransform = originalLocalCollection.wrapTransform;
+LocalCollection._f = originalLocalCollection._f;
+LocalCollection._compileProjection = originalLocalCollection._compileProjection;
+LocalCollection._binarySearch = originalLocalCollection._binarySearch;
+
+console.log("LocalCollection._f", LocalCollection._f)
 
 // Currently server and client side behaves differently when counting with skip. So we make it
 // behave the same for tests. See https://github.com/meteor/meteor/issues/1201
@@ -58,6 +66,9 @@ Mongo.Collection.prototype.update = function (selector, mod, options, callback) 
   if (selector && selector['a.b'] && selector['a.b'].$near) {
     this._ensureIndex({'a.b': '2d'});
   }
+  if (selector && selector['a.c'] && selector['a.c'].$near) {
+    this._ensureIndex({'a.c': '2d'});
+  }
 
   return originalUpdate.apply(this, arguments);
 };
@@ -65,25 +76,27 @@ Mongo.Collection.prototype.update = function (selector, mod, options, callback) 
 var IGNORED_TESTS = [
   // Tests which do not test any reactive behavior, just Minimongo specifics,
   // and use code which does not exist on the server.
-  'minimongo - misc',
-  'minimongo - projection_compiler',
-  'minimongo - fetch with projection, deep copy',
-  'minimongo - ordering',
-  'minimongo - binary search',
   'minimongo - saveOriginals',
   'minimongo - saveOriginals errors',
   'minimongo - pause',
   'minimongo - ids matched by selector',
+  'minimongo - lookup',
+
+  // Pending pull requests.
+  // See: https://github.com/meteor/meteor/pull/9539
+  'minimongo - cursors',
 
   // Fail because of difference between Minimongo and server. For some of these
   // tests (those with autorun) we have a fixed version in tests.js.
-  // See https://github.com/meteor/meteor/issues/3527
-  'minimongo - basics',
-  // See https://github.com/meteor/meteor/issues/5165
-  // See https://github.com/meteor/meteor/issues/5166
-  // See https://github.com/meteor/meteor/issues/5167
+  'minimongo - sort function',
+  'minimongo - cannot $set with null bytes',
+  'minimongo - cannot insert using invalid field names',
+  'minimongo - $near operator tests',
+  'minimongo - $near and $geometry for legacy coordinates',
   'minimongo - modify',
-  // See https://github.com/meteor/meteor/issues/3597
+  // See: https://github.com/meteor/meteor-feature-requests/issues/252
+  'minimongo - basics',
+  // See: https://github.com/meteor/meteor/issues/3597
   'minimongo - observe ordered',
   'minimongo - observe ordered: true',
   'minimongo - observe ordered: false',
@@ -91,9 +104,8 @@ var IGNORED_TESTS = [
   'minimongo - reactive stop',
   'minimongo - fetch in observe',
   'minimongo - count on cursor with limit',
+  'minimongo - reactive skip/limit count while updating',
   'minimongo - fine-grained reactivity of query with fields projection',
-  // See https://github.com/meteor/meteor/issues/3599
-  'minimongo - $near operator tests'
 ];
 
 var originalTinytestAdd = Tinytest.add;
